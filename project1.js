82b78b20bba182728c5618f8d203d4adccbd3abf
jkabore <jkabore1@gmail.com>
2018-03-20 19:33:09 -0400

    Add files via upload

---
+++ project1.js
@@ -0,0 +1,18 @@
+$("#btn").on("click", function(){
+    var queryURL="https://developers.google.com/custom-search/";
+    queryURL+="?AIzaSyBRCMSBy9r1fHzujH3881ZnaEkfS8iYwz8";
+    $.ajax({
+        url:queryURL;
+    })
+
+
+
+
+
+
+
+
+
+
+
+});
