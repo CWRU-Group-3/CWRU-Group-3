cb4fd7612eb3f4504e4b3935776f501a52cbcfa0
jkabore <jkabore1@gmail.com>
2018-03-22 19:00:10 -0400

    Add files via upload

--- project1.js
+++ project1.js
@@ -1,21 +1,19 @@
 var APIKey = "&appid=415a66a0a17d32018d6c3cc274b84af9";
-
-
-
+        
 //function that will collect the user input and return the weather details
 
-$("#bttn").click(function (event) {
+$("#search-button").click(function (event) {
 ////prevent from submiting
-event.preventDefault();
-$("#spicy").empty();
-var searchBox = $("#spicy").val().trim();
-//adding the city name that the user will type to the queryURL
-var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + searchBox + APIKey;
-console.log(searchBox);
+    event.preventDefault();
+    $("#country-input").empty();
+    var searchBox = $("#country-input").val().trim();
+    //adding the city name that the user will type to the queryURL
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q="+searchBox+APIKey;
+  console.log(searchBox);
 
 
-
-$.ajax({
+    
+    $.ajax({
     url: queryURL,
     method: "GET"
 
@@ -23,10 +21,10 @@ $.ajax({
 
     console.log(queryURL);
     console.log(response);
-    $(".city").text(response.name + " " + "weather details");
-    $(".wind").text("Wind Speed: " + response.wind.speed);
-    $(".humidity").text("Humidity: " + response.main.humidity);
-    $(".temp").text("Temperature (F) " + response.main.temp);
-})
-
+    $("#_weather").text(response.name + " " + "weather details");
+    $("#_windt").text("Wind Speed: " + response.wind.speed);
+    $("#_humidity").text("Humidity: " + response.main.humidity);
+    $("#_temperature").text("Temperature (F) " + response.main.temp);
+    })
+   
 })
