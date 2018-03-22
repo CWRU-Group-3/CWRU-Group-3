eb5abe17f50602f5359e0c05cbbe3b69b80a468d
machartman <mlh143@case.edu>
2018-03-22 19:33:28 -0400

    Changed my div portion of page to work with the file youtube.js. Also created youtube.js and got the video to show up when button is pressed

--- country-search.js
+++ country-search.js
@@ -23,6 +23,7 @@
            for (j=0; j<3; j++) {
 
            var newButton = $("<button>").attr("value", countriesArr[j]).text(countriesArr[j]);
+           newButton.attr("class", "city");
            $("#button-container").append(newButton);
     } 
     // if (response.geonames[i].countryName != countrySearch) {
